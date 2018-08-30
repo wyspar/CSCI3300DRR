bd2232a5a228cc4f0cbd1d7289af44cb7e934cd1
wyspar <supertrack69@gmail.com>
2018-08-30 14:59:01 -0400

    third update

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Goodbye world.");
+        primaryStage.setTitle("Goodbye worlds xd.");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
