038635668903298dd2a08acc95a552cf99a8d8f3
wyspar <supertrack69@gmail.com>
2018-08-30 15:14:54 -0400

    fourth update

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,12 @@
 package sample;
 
+import javafx.fxml.FXML;
+
 public class Controller {
+    @FXML
+   // TextField email, username, password;
+    public void touchLogin(){
+        Model model = new Model();
+
+    }
 }
